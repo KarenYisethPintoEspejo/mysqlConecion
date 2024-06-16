c1796bf3741f0d8655a2ee47f64d20c7f99604ba
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com>
2024-06-16 17:38:17 -0500

    feat: :sparkles: Creacion de modulos para consultas

--- helpers/conexion.js
+++ helpers/conexion.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 export const connection = await mysql.createConnection({
   host: '172.16.101.146',
   user: 'camper',
-  database: 'trainer',
+  database: 'm2_karenyisethpintoespejo',
   port: 3306,
   password: 'campus2023'
 });
