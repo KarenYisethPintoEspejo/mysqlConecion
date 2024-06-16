7b5d6da0e9894e8accd2bcaadf5801c1b3feddde
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com>
2024-06-16 18:14:03 -0500

    feat: :sparkles: Consulta numero 3 realizada

--- js/module/customers.js
+++ js/module/customers.js
@@ -1,11 +1,19 @@
 import { connection } from "../../helpers/conexion.js";
 
-export const getAllLastNameASC = async()=>{
-    const [result] = await connection.query(`SELECT contactLastname, contactFirstname FROM customers ORDER BY contactLastname`);
-    return result;
-}
+// export const getAllLastNameASC = async()=>{
+//     const [result] = await connection.query(`SELECT contactLastname, contactFirstname FROM customers ORDER BY contactLastname`);
+//     return result;
+// }
 
-export const getAllLastNameDESC = async()=>{
-    const [result] = await connection.query(`SELECT contactLastname, contactFirstname FROM customers ORDER BY contactLastname DESC`);
+// export const getAllLastNameDESC = async()=>{
+//     const [result] = await connection.query(`SELECT contactLastname, contactFirstname FROM customers ORDER BY contactLastname DESC`);
+//     return result;
+// }
+
+
+// 3. **Detalles de un cliente específico (por ejemplo, customerNumber = 101):**
+
+export const getSpecificCustomer = async ({customerNumber} = {customerNumber: 496})=>{
+    const [result] = await connection.execute(`select customerNUmber, customerName, contactFirstName, contactLastName, phone from customers where customerNumber = ?`, [customerNumber]);
     return result;
 }
--- main.js
+++ main.js
@@ -14,11 +14,12 @@
 
 
 import { getAllProductsStock } from "./js/module/product.js";
-
 import { getEmployeesSpecificOffice } from "./js/module/employees.js";
+import { getSpecificCustomer } from "./js/module/customers.js";
 
 
 
 
 console.log(await getAllProductsStock());  //Consulta 1
 console.log(await getEmployeesSpecificOffice({office:1}));  //Consulta 2
+console.log(await getSpecificCustomer({customerNumber: 496}))  //Consulta 3
